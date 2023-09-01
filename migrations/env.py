cfc85a46264254055e42fe1bfb345dde2a609e4d
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-09-01 17:35:54 +0400

    Вынес все чувствительные данные в .env, внесли изменения в database, создали config

--- migrations/env.py
+++ migrations/env.py
@@ -8,7 +8,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from app.database import Base, DATABASE_URL
+from app.config import settings
+from app.database import Base
 from app.blogs.models import Blogs
 from app.users.models import Users
 
@@ -19,7 +20,7 @@ sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 
 config = context.config
 
-config.set_main_option("sqlalchemy.url", f"{DATABASE_URL}?async_fallback=True")
+config.set_main_option("sqlalchemy.url", f"{settings.DATABASE_URL}?async_fallback=True")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
